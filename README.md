ba873d47a4e335b8875706ec874466662b414427
Tom King <mobilemind@pobox.com>
2014-11-06 18:13:31 -0800

    initial commit

---
+++ README.md
@@ -0,0 +1,49 @@
+ToDo
+====
+This is an Android Application for the To Do List.
+
+Time Spent (Coding): 2 hours +
+
+Time Spent (Setup): 30 mins (had setup MUCH older Eclipse/ADK on another machine so it was familiar)
+
+Completed User Stories:
+
+* [ ] First Version of the App, [ToDoActivity] a list of ToDo Items Saved to file that can have items added to it (text field and Add button) or removed from it (long click)
+* [ ] Edit feature added to the First Version, a click of an item in To Do list leads to the next activity [EditItemActivity] where you can edit that item and then Save and return to the [ToDoActivity] where your changes will be reflected and saved to file.
+
+
+Walkthrough of all user stories:
+
+![GIF Walkthrough](nameOfGif.gif)
+
+
+Styling Modifications:
+
+* [ ] Added a drawable for the buttons, with a pink gradient background coloring.
+* [ ] Changed the app logo to an elephant
+* [ ] Changed the app to be centered around the idea that an elephant never forgets
+* [ ] Modified the text field and button width at the top of the Edit Activity [EditItemActivity] and added the elephant image again
+* [ ] Remove the unused dropdown menu from the Activity Bar in the app.
+
+Walkthrough of the user stories with add ons:
+
+![GIF Walkthrough](nameOfGif.gif)
+
+Basic Priorities (without styling):
+
+* [ ] Added a custom ArrayAdapter to support priority information
+* [ ] Added a custom class (`ToDoItem`) to help with representing the data in the to do list
+* [ ] Added Checkbox to Edit View to allow users to change the priority of a to do item.
+
+Walkthrough of the user stories with basic priorities:
+
+![Video Walkthrough](nameOfGif.gif)
+
+Started Working on Adding some Styling to the App with Basic Priorities:
+
+* [ ] Modified some background drawables and colors and tinkered with padding on elements to create a different look and feel to the to do list activity.
+
+Walkthrough of the user stories with some styling.
+
+
+GIFs created by LiceCap <http://www.cockos.com/licecap/>
---
+++ src/com/yahoo/tomking/simpletodo/ToDoActivity.java
@@ -0,0 +1,107 @@
+ package com.yahoo.tomking.simpletodo;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+
+import org.apache.commons.io.FileUtils;
+
+import android.app.Activity;
+import android.media.AudioManager;
+import android.media.ToneGenerator;
+import android.os.Bundle;
+import android.view.KeyEvent;
+import android.view.View;
+import android.view.View.OnKeyListener;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemLongClickListener;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.ListView;
+
+public class ToDoActivity extends Activity {
+	private ArrayList<String> todoItems;
+	private ArrayAdapter<String> todoAdapter;
+	private ListView lvItems;
+	private EditText etNewItem;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_to_do);
+        etNewItem = (EditText) findViewById(R.id.etNewItem);
+        lvItems = (ListView) findViewById(R.id.lvItems);
+        readItems();
+        todoAdapter= new ArrayAdapter<String>(this,
+        		android.R.layout.simple_list_item_1, todoItems);
+        lvItems.setAdapter(todoAdapter);
+        setupListViewListner();
+        setupEditTextViewListener();
+    }
+
+	private void setupListViewListner() {
+		lvItems.setOnItemLongClickListener(new OnItemLongClickListener() {
+			@Override
+			public boolean onItemLongClick(AdapterView<?> adapter, View items,
+					int pos, long id) {
+				todoItems.remove(pos);
+				todoAdapter.notifyDataSetChanged();
+				writeItems();
+				return false;
+			}
+		});
+	}
+	
+	private void setupEditTextViewListener() {
+		etNewItem.setOnKeyListener(new OnKeyListener() {
+			@Override
+			public boolean onKey(View v, int keyCode, KeyEvent event) {
+				if (event.getKeyCode() == KeyEvent.KEYCODE_ENTER && event.getAction() == KeyEvent.ACTION_UP) {
+					String itemText = etNewItem.getText().toString();
+					if ("" != itemText.replaceAll("[\n\r]","")) {
+						// strip new line before appending item added by pressing Enter
+						etNewItem.setText(itemText.toCharArray(), 0, itemText.length()- 1);
+					}
+					onAddedItem(v);
+					return true;
+				}
+				return false;
+			}
+		});
+	}
+	
+	public void onAddedItem(View v) {
+		String itemText = etNewItem.getText().toString();
+		if ( "" == itemText.replaceAll("[\n\r]","")) {
+			// beep on null entry
+			final ToneGenerator tg = new ToneGenerator(AudioManager.STREAM_NOTIFICATION, 100);
+		    tg.startTone(ToneGenerator.TONE_PROP_BEEP);
+		}
+		else {
+			// update view adapter and file store
+			todoAdapter.add(itemText);
+			writeItems();
+		}
+		etNewItem.setText("");
+	}
+	
+	private void readItems() {
+		File filesDir = getFilesDir();
+		File todoFile = new File(filesDir, "todo.text");
+		try {
+			todoItems= new ArrayList<String>(FileUtils.readLines(todoFile));
+		} catch (IOException e) {
+			todoItems= new ArrayList<String>();
+		}
+	}
+	
+	private void writeItems() {
+		File filesDir = getFilesDir();
+		File todoFile = new File(filesDir, "todo.text");
+		try {
+			FileUtils.writeLines(todoFile, todoItems);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+}
